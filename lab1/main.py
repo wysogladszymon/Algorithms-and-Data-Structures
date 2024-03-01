c55c75f4099210647adf3399e407820211ff959c
wysogladszymon <wysogladszymon@gmail.com>
2024-03-01 08:02:31 +0100

    end of lab 1

--- lab1/main.py
+++ lab1/main.py
@@ -1,4 +1,5 @@
 from matrix import Matrix
+from chio_det import det
 
 def main():
   m1 = Matrix(
@@ -10,5 +11,20 @@ def main():
   print(m1 + m2,'\n')
   print(m1 * m3)
 
+  m4 = Matrix([[5,1,1,2,3],
+              [4,2,1,7,3],
+              [2,1,2,4,7],
+              [9,1,0,7,0],
+              [1,4,7,2,2]])
+  
+  m5 = Matrix([[0,1,1,2,3],
+            [4,2,1,7,3],
+            [2,1,2,4,7],
+            [9,1,0,7,0],
+            [1,4,7,2,2]])
+  print(det(m4))
+  print(det(m5))
+
 if __name__ == '__main__':
   main()
+
